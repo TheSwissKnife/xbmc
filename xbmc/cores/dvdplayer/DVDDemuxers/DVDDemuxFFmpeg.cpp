ab482944a9b889181d34e058fe8d250c03c38da7
TheSwissKnife <adam@ubuntu.(none)>
2011-06-09 16:41:34 -0700

    ffmpeg demux zero pts fix

--- xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
+++ xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
@@ -714,10 +714,11 @@ DemuxPacket* CDVDDemuxFFmpeg::Read()
         // lavf sometimes bugs out and gives 0 dts/pts instead of no dts/pts
         // since this could only happens on initial frame under normal
         // circomstances, let's assume it is wrong all the time
-        if(pkt.dts == 0)
-          pkt.dts = AV_NOPTS_VALUE;
-        if(pkt.pts == 0)
-          pkt.pts = AV_NOPTS_VALUE;
+//        if(pkt.dts == 0)
+//         pkt.dts = AV_NOPTS_VALUE;
+// do we really need to still do this it means we lose first packet
+//        if(pkt.pts == 0)
+//          pkt.pts = AV_NOPTS_VALUE;
 
         if(m_bMatroska && stream->codec && stream->codec->codec_type == AVMEDIA_TYPE_VIDEO)
         { // matroska can store different timestamps
